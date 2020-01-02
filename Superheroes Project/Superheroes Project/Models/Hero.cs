7c1d1eb882a7adac0a9720c071ff02530e06beaf
Erinn Neidinger <erinndneidinger@gmail.com>
2020-01-02 09:56:46 -0600

    created Superheroes repo

---
+++ Superheroes Project/Superheroes Project/Models/Hero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Superheroes_Project.Models
+{
+    public class Hero
+    {
+        [Key]
+        public int Id { get; set; }
+        public int firstName { get; set; }
+        public int lastName { get; set; }
+    }
+}
